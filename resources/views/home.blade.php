5888a02b3f87a7b5ee4af02053cbaf809a84d276
alessiaBer <alexdeyn@gmail.com>
2023-05-25 18:37:10 +0200

    [add] style to cancelled trains

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,18 +7,28 @@
         <thead>
             <tr>
                 <th scope="col">Train Code</th>
-                <th scope="col">Time</th>
-                <th scope="col">Company</th>
+                <th scope="col">Departure Time</th>
                 <th scope="col">Arrival Station</th>
+                <th scope="col">Arrival Time</th>
+                <th scope="col">Company</th>
             </tr>
         </thead>
         <tbody>
             @foreach($trains as $train)
             <tr>
+                @if($train->cancelled === 0) 
                 <th scope="row">{{$train->train_code}}</th>
                 <td>{{date('H:i', strtotime($train->departure_time))}}</td>
-                <td>{{$train->company}}</td>
                 <td>{{$train->arrival_station}}</td>
+                <td>{{date('H:i', strtotime($train->arrival_time))}}</td>
+                <td>{{$train->company}}</td>
+                @else 
+                <th class="bg-danger" scope="row">{{$train->train_code}}</th>
+                <td class="bg-danger dep_fail">{{date('H:i', strtotime($train->departure_time))}}<span class="d-inline-block ps-3">CANCELLED</span></td>
+                <td class="bg-danger">{{$train->arrival_station}}</td>
+                <td class="bg-danger">{{date('H:i', strtotime($train->arrival_time))}}</td>
+                <td class="bg-danger">{{$train->company}}</td>
+                @endif
             </tr>
             @endforeach
         </tbody>
